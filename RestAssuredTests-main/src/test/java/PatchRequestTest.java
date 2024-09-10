3c9869e40992dfd622f38a6c2564a472992f5035
OksanaOnduke <oksanarybalko@yandex.ru>
2024-09-10 20:29:30 +0300

    Update PatchRequestTest.java

--- RestAssuredTests-main/src/test/java/PatchRequestTest.java
+++ RestAssuredTests-main/src/test/java/PatchRequestTest.java
@@ -9,18 +9,16 @@ public class PatchRequestTest extends BaseTest {
     public void testPatchRequest() {
         String requestBody = "This is expected to be sent back as part of response body.";
 
-        // Выполняем PATCH-запрос с телом
         Response response = given()
                 .body(requestBody)
                 .when()
                 .patch("/patch")
                 .then()
-                .statusCode(200)  // Проверяем, что статус-код 200
-                .body("data", equalTo(requestBody))  // Проверяем, что тело ответа совпадает с отправленным телом
+                .statusCode(200)  
+                .body("data", equalTo(requestBody))  
                 .extract()
                 .response();
 
-        // Выводим тело ответа для наглядности (опционально)
         System.out.println("Response: " + response.asString());
     }
 }
